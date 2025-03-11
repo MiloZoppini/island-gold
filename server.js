9105a984a36d7743cfadf4df0a5ff5026901d59e
MiloZoppini <milozoppini@gmail.com>
2025-03-11 09:00:34 +0100

    Risolto problema di caricamento di Three.js e aggiunto server Express

--- server.js
+++ server.js
@@ -2,16 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Servi i file statici dalla directory public
-app.use(express.static('public'));
+// Serve i file statici dalla directory 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Gestisci tutte le altre richieste reindirizzandole a index.html
+// Reindirizza tutte le richieste a index.html (utile per SPA)
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Avvia il server sulla porta specificata da Render.com o sulla 3000 di default
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server in esecuzione sulla porta ${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server in ascolto sulla porta ${port}`);
 }); 
