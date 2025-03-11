fba6192068c4f1cdf050ab13daa314e8c3986b75
MiloZoppini <milozoppini@gmail.com>
2025-03-11 09:04:50 +0100

    Sostituiti moduli ES6 con versioni UMD e aggiunto socket.io

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 // Serve i file statici dalla directory 'public'
 app.use(express.static(path.join(__dirname, 'public')));
@@ -10,7 +12,16 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Gestione delle connessioni socket.io
+io.on('connection', (socket) => {
+  console.log('Un utente si è connesso');
+
+  socket.on('disconnect', () => {
+    console.log('Un utente si è disconnesso');
+  });
+});
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`Server in ascolto sulla porta ${port}`);
 }); 
